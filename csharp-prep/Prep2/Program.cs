8581f8b714c44cc9197672ac57ec24f1d59cce41
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-11 22:37:25 +0100

    Core Req 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradeInput = Console.ReadLine();
+
+        int grade = int.Parse(gradeInput);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A.");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is a B.");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is a C.");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is a D.");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is an F.");
+        }
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
