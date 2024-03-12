d87d60092bd0447c2238640e73f9aefae67885ee
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-12 22:02:40 +0100

    Core req. 3 update

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,31 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
+      
         Console.WriteLine("What is your grade percentage? ");
         string gradeInput = Console.ReadLine();
 
+        // Declear a variable to store the parsed input
+        //int grade;
+
+        // Parse the input and store it in the variable grade
         int grade = int.Parse(gradeInput);
+
+        //create a new variable called 'letter' to store the letter grade
+        char letter;
 
         if (grade >= 90)
         {
-            Console.WriteLine("Your grade is an A.");
+            letter = 'A';
         }
         else if (grade >= 80)
         {
-            Console.WriteLine("Your grade is a B.");
+            letter = 'B';
         }
         else if (grade >= 70)
         {
-            Console.WriteLine("Your grade is a C.");
+            letter = 'C';
         }
         else if (grade >= 60)
         {
-            Console.WriteLine("Your grade is a D.");
+            letter = 'D';
         }
         else
         {
-            Console.WriteLine("Your grade is an F.");
+            letter = 'F';
         }
+
+        Console.WriteLine($"Your grade is: {letter}");
 
         if (grade >=70)
         {
