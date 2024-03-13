83510dcbaaf84359d2fa0d7ca0749e715875d2c6
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-13 15:53:18 +0100

    prep 3 guess number game where the user enter the number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Ask the user to input the magic number and their guess.
+        Console.Write("What is the magic number? ");
+        string magicNumberInput = Console.ReadLine();
+
+        Console.Write("What is your guess? ");
+        string guessInput = Console.ReadLine();
+
+        // Parse the inputs and store it in the variables magicNumber and guess.
+        int magicNumber = int.Parse(magicNumberInput);
+        int guess = int.Parse(guessInput);
+
+        // Determine if the user needs to guess higher or lower
+        if (magicNumber > guess)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
+        // A loop that keeps looping until the user guess right
+        while (magicNumber != guess)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
+
     }
 }
