a72a66813ef01068e2355ed0f1cfe163826d0d9b
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-20 20:23:15 +0100

    prep 1 update

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name? ");
+        Console.WriteLine("What's your first name? ");
         string firstName = Console.ReadLine();
 
-        Console.WriteLine("What is your last name? ");
+        Console.WriteLine("What's your last name? ");
         string lastName = Console.ReadLine();
 
         Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
