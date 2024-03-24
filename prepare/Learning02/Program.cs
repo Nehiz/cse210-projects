b59a346f5fb716e4dfbbe1c0a06e167ac6244d71
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-24 19:03:23 +0100

    learning 02 completed

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,50 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create a new job instance named job1
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Display the company of the job above
+        //Console.WriteLine(job1._company);
+        
+        // Call the method to display the job1 details
+        // job1.Display();
+
+        // Create a second job instance named job2
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // Display the company of the job2
+        //Console.WriteLine(job2._company);
+        
+        // Call the method to display the job2 details
+        // job2.Display();
+
+        // Write a space before displaying the resume
+        Console.WriteLine();
+
+        // Create the Resume class
+        Resume myResume = new Resume();
+        myResume._personName = "Nehikhare Efehi";
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+        myResume.Display();
+
+        // Write a space after displaying the resume
+        Console.WriteLine();
+       
+
+
+
+      
+
     }
+
 }
