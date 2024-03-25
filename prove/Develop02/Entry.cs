5d9d976c963359ff4c6e28e013da9452fe00e64e
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-25 17:13:53 +0100

    Entry class (), New ppty to handle additional info added

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -5,17 +5,20 @@ public class Entry
     public string Prompt { get; set; }
     public string Response { get; set; }
     public DateTime Date { get; set; }
+    // New property to handle additional information from user
+    public string AdditionalInfo { get; set; }
 
-    public Entry(string prompt, string response)
+    public Entry(string prompt, string response, string additionalInfo)
     {
         Prompt = prompt;
         Response = response;
         Date = DateTime.Now;
+        AdditionalInfo = additionalInfo;
     }
 
     public override string ToString()
     {
-        return $"Date: {Date}\nPrompt: {Prompt}\nResponse: {Response}\n";
+        return $"Date: {Date}\nPrompt: {Prompt}\nResponse: {Response}\nAdditional Info: {AdditionalInfo}";
     }
 
 }
