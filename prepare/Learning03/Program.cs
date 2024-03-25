a2750d673d553c1c1263afd93fa81e0329fcfecc
Circuit19 <efehi.nehikhare@gmail.com>
2024-03-25 22:47:51 +0100

    Program class with instances to test the fraction class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Create instances of Fraction class using different constructors
+        Fraction fraction1 = new Fraction();
+        Fraction fraction2 = new Fraction(5);
+        Fraction fraction3 = new Fraction(3, 4);
+        Fraction fraction4 = new Fraction(1, 3);
+
+        // set values using setters
+        fraction1.TopNumber = 1;
+        fraction1.BottomNumber = 1;
+
+        // Get values using getters
+        int top = fraction1.TopNumber;
+        int bottom = fraction1.BottomNumber;
+
+        // Display the fraction as a string and its decimal value
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
     }
 }
